f9f29c19cd22dc3cd417d46cb356916abeb429a8
boryam <borys.mohyla@ria.com>
2017-02-23 19:47:22 +0200

    init

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-
+const fs = require('fs');
 (async function dfd() {
-    console.log('test');
+    let text = await fs.readFileSync('./data.in', 'utf8');
+    let array = text.split('\n');
+    array.push('111 3 2');
+    await fs.writeFileSync('./data.out', array.join('\n'))
 })();
